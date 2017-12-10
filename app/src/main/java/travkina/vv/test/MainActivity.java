b0855690828caca8aec1653aae0d14292faf5564
Анастасия <ameneksia@gmail.com>
2017-12-10 18:00:09 +0300

    №1

--- app/src/main/java/travkina/vv/test/MainActivity.java
+++ app/src/main/java/travkina/vv/test/MainActivity.java
@@ -4,23 +4,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    int i = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.button);
-         button.setOnClickListener(this);
+        ImageView imageView = (ImageView) findViewById(R.id.image);
+        imageView.setImageResource(R.drawable.a);
+
+        imageView.setOnClickListener(this);
+
     }
 
 
 
     @Override
     public void onClick(View v) {
-        Toast tost = Toast.makeText(this,R.string.text, Toast.LENGTH_LONG);
-        tost.show();
+
+        ImageView imageView =  (ImageView) findViewById(R.id.image);
+        if(i%2==0) {
+            imageView.setImageResource(R.drawable.q);
+        }
+        else{
+            imageView.setImageResource(R.drawable.a);
+        }
+        i++;
     }
+
+
 }
